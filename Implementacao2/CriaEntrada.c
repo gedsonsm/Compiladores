7ebd19e93c8126970d23b92c0c6583fd6ad10e27
gedsonsm <gedson-mello@hotmail.com>
2015-09-08 00:23:02 -0300

    a

---
+++ Implementacao2/CriaEntrada.c
@@ -0,0 +1,33 @@
+// Gerador para K e S
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numY = 200;
+    printf("Numero de repeticoes de Y: ");
+    scanf("%d", &numY);
+    int i=0;
+    char valordeY[1000] = "S'(C'BI(SII))I(B(C'BI(SII))I)";
+
+    FILE *entradagerada;
+    entradagerada = fopen ("string.in", "w");
+
+    for(i = 0 ; i < numY-2 ; i++)
+    {
+        fputs(valordeY,entradagerada);
+        fputs("((",entradagerada);
+    }
+        fputs(valordeY,entradagerada);
+        fputs("((",entradagerada);
+        fputs(valordeY,entradagerada);
+        fputs("(KK))K)",entradagerada);
+
+    for(i = 0 ; i < numY-2 ; i++)
+    {
+        fputs(")K)",entradagerada);
+
+    }
+
+    return 0;
+}
