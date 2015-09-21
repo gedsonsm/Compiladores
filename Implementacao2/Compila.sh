c5823f07b250aacf0f9286ca985213506943e2a1
gedsonsm <gedson-mello@hotmail.com>
2015-09-21 16:30:21 -0300

    Update Compila.sh

--- Implementacao2/Compila.sh
+++ Implementacao2/Compila.sh
@@ -2,9 +2,11 @@
 
 gcc -c Codigos/CriaEntrada.c
 gcc -o Cria CriaEntrada.o
-./Cria 1000
+./Cria 200
 gcc -c Codigos/main.c
 gcc -c Codigos/operacoesComStr.c
 gcc -c Codigos/reducoes.c
-gcc -o main main.o operacoesComStr.o reducoes.o
-rm main.o Cria CriaEntrada.o operacoesComStr.o reducoes.o
+gcc -O4 -o main main.o operacoesComStr.o reducoes.o
+wait 
+time ./main
+rm main main.o Cria CriaEntrada.o operacoesComStr.o reducoes.o
