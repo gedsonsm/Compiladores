131262721f74174aacff6d8595a8bf38f9db04d0
CarolPM <cpm@cin.ufpe.br>
2015-09-28 03:32:52 -0300

    Update reducoes.h

--- Implementacao3/reducoes.h
+++ Implementacao3/reducoes.h
@@ -8,6 +8,11 @@
 
 struct Lista S(struct Lista *pLista, int *parou);
 struct Lista K(struct Lista *pLista, int *parou);
-
+struct Lista I(struct Lista *pLista, int *parou);
+struct Lista B(struct Lista *pLista, int *parou);
+struct Lista C(struct Lista *pLista, int *parou);
+struct Lista SH(struct Lista *pLista, int *parou);
+struct Lista BH(struct Lista *pLista, int *parou);
+struct Lista CH(struct Lista *pLista, int *parou);
 
 #endif
