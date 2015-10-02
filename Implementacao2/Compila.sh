15d01e444e642b6fd34ccd888ab787f11a9013ff
CarolPM <cpm@cin.ufpe.br>
2015-10-02 01:49:28 -0300

    Update Compila.sh

--- Implementacao2/Compila.sh
+++ Implementacao2/Compila.sh
@@ -2,7 +2,7 @@
 
 gcc Codigos/CriaEntrada_KS.c -o Cria
 #gcc Codigos/CriaEntrada_Turner.c -o Cria
-./Cria 132
+./Cria 1000
 gcc -c Codigos/main.c
 gcc -c Codigos/operacoesComStr.c
 gcc -c Codigos/reducoes.c
