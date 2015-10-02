a18c7b291d9d97e58f42ecea7f18f24b5dbc112c
gedsonsm <gedson-mello@hotmail.com>
2015-10-02 00:34:43 -0300

    Update Compila.sh

--- Implementacao2/Compila.sh
+++ Implementacao2/Compila.sh
@@ -10,3 +10,10 @@ gcc -O4 -o main main.o operacoesComStr.o reducoes.o
 wait 
 time ./main
 rm main main.o Cria operacoesComStr.o reducoes.o
+
+
+
+
+#gcc −pg c o d i g o_ f o n t e . c −o p rog rama
+#. / p rog rama
+#g p r o f p rog rama gmon . o u t > l o g . t x t
